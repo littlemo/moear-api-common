23377a1fce8efe958088f64d34d0d59037763706
Moore.Huang <moore@moorehy.com>
2018-03-10 11:56:15 +0800

    F: 从KindleEar移植插入OEB目录的工具方法

--- moear_api_common/base.py
+++ moear_api_common/base.py
@@ -1,5 +1,6 @@
 import abc
 import six
+import re
 
 from . import config
 
@@ -87,6 +88,170 @@ class PackageBase(object):
         即优先级为：Spider自定义 > 具体Package配置 > Package全局默认配置
         """
         self.settings.update(settings)
+
+    def insert_toc(
+            self, oeb, sections, toc_thumbnails,
+            insertHtmlToc=True, insertThumbnail=True):
+        """
+        插入OEB目录
+
+        此处移植的 KindleEar 中相关工具方法，再次感谢原作者。
+
+        此工具方法用于创建 OEB 的两级目录。
+
+        :params oeb: 待操作的 OEB 对象
+        :params sections dict: 待操作的书籍数据结构有序字典，
+            值为元组 (title, brief, humbnail, content)
+        :params toc_thumbnails dict: key为图片原始URL，value为其在 OEB 内的 href
+        :params insertHtmlToc bool: 是否插入 HTML 目录
+        :params insertThumbnail bool: 是否插入缩略图
+        """
+        toc_title = self.settings.get('toc_title')
+        css_pat = r'<style type="text/css">(.*?)</style>'
+        css_ex = re.compile(css_pat, re.M | re.S)
+        body_pat = r'(?<=<body>).*?(?=</body>)'
+        body_ex = re.compile(body_pat, re.M | re.S)
+
+        num_articles = 1
+        num_sections = 0
+
+        ncx_toc = []
+        # html_toc_2 secondary toc
+        html_toc_2 = []
+        name_section_list = []
+        for sec in sections.keys():
+            css = [
+                '.pagebreak{page-break-before:always;}h1{font-size:2.0em;}h2{'
+                'font-size:1.5em;}h3{font-size:1.4em;}h4{font-size:1.2em;}h5{'
+                'font-size:1.1em;}h6{font-size:1.0em;}']
+            html_content = []
+            secondary_toc_list = []
+            first_flag = False
+            sec_toc_thumbnail = None
+            for title, brief, thumbnail, content in sections[sec]:
+                # 获取自定义的CSS
+                for css_obj in css_ex.finditer(content):
+                    if css_obj and css_obj.group(1):
+                        if css_obj.group(1) not in css:
+                            css.append(css_obj.group(1))
+
+                if first_flag:
+                    # insert anchor && pagebreak
+                    html_content.append(
+                        '<div id="%d" class="pagebreak">' % (num_articles))
+                else:
+                    # insert anchor && pagebreak
+                    html_content.append('<div id="%d">' % (num_articles))
+                    first_flag = True
+                    if thumbnail:
+                        sec_toc_thumbnail = thumbnail  # url
+
+                # 将body抽取出来
+                body_obj = re.search(body_ex, content)
+                if body_obj:
+                    # insect article
+                    html_content.append(body_obj.group() + '</div>')
+                    secondary_toc_list.append(
+                        (title, num_articles, brief, thumbnail))
+                    num_articles += 1
+                else:
+                    html_content.pop()
+            html_content.append('</body></html>')
+
+            html_content.insert(
+                0, '<html><head><title>%s</title><style type="text/css">%s</st'
+                'yle></head><body>' % (sec, ''.join(css)))
+
+            # add section.html to maninfest and spine
+            # We'd better not use id as variable.
+            # It's a python builtin function.
+            id_, href = oeb.manifest.generate(
+                id='feed', href='feed%d.html' % num_sections)
+            item = oeb.manifest.add(
+                id_, href, 'application/xhtml+xml', data=''.join(html_content))
+            oeb.spine.add(item, True)
+
+            # 在目录分类中添加每个目录下的文章篇数
+            sec_with_num = '%s (%d)' % (sec, len(sections[sec]))
+            # Sections name && href && no brief
+            ncx_toc.append(
+                ('section', sec_with_num, href, '', sec_toc_thumbnail))
+
+            # generate the secondary toc
+            if insertHtmlToc:
+                html_toc_ = [
+                    '<html><head><title>toc</title></head>'
+                    '<body><h2>%s</h2><ol>' % (sec_with_num)]
+            for title, anchor, brief, thumbnail in secondary_toc_list:
+                if insertHtmlToc:
+                    html_toc_.append(
+                        '&nbsp;&nbsp;&nbsp;&nbsp;<li><a href="%s#%d">%s</a>'
+                        '</li><br />' % (href, anchor, title))
+                # article name & article href && article brief
+                ncx_toc.append((
+                    'article', title, '%s#%d' % (href, anchor),
+                    brief, thumbnail))
+            if insertHtmlToc:
+                html_toc_.append('</ol></body></html>')
+                html_toc_2.append(html_toc_)
+                name_section_list.append(sec_with_num)
+
+            num_sections += 1
+
+        if insertHtmlToc:
+            # Generate HTML TOC for Calibre mostly
+            # html_toc_1 top level toc
+            html_toc_1 = [
+                u'<html><head><title>Table Of Contents</title></head><body>'
+                '<h2>%s</h2><ul>' % (toc_title)]
+            html_toc_1_ = []
+            # We need index but not reversed()
+            for a in xrange(len(html_toc_2) - 1, -1, -1):
+                # Generate Secondary HTML TOC
+                id_, href = oeb.manifest.generate(
+                    id='section', href='toc_%d.html' % (a))
+                item = oeb.manifest.add(
+                    id_, href, 'application/xhtml+xml',
+                    data=" ".join(html_toc_2[a]))
+                oeb.spine.insert(0, item, True)
+                html_toc_1_.append(
+                    '&nbsp;&nbsp;&nbsp;&nbsp;<li><a href="%s">%s</a>'
+                    '</li><br />' % (href, name_section_list[a]))
+            html_toc_2 = []
+            for a in reversed(html_toc_1_):
+                html_toc_1.append(a)
+            html_toc_1_ = []
+            html_toc_1.append('</ul></body></html>')
+            # Generate Top HTML TOC
+            id_, href = oeb.manifest.generate(id='toc', href='toc.html')
+            item = oeb.manifest.add(
+                id_, href, 'application/xhtml+xml', data=''.join(html_toc_1))
+            oeb.guide.add('toc', 'Table of Contents', href)
+            oeb.spine.insert(0, item, True)
+
+        # Generate NCX TOC for Kindle
+        po = 1
+        toc = oeb.toc.add(unicode(
+            oeb.metadata.title[0]), oeb.spine[0].href,
+            id='periodical', klass='periodical', play_order=po)
+        po += 1
+        for ncx in ncx_toc:
+            if insertThumbnail and ncx[4]:
+                toc_thumbnail = toc_thumbnails[ncx[4]]
+            else:
+                toc_thumbnail = None
+
+            if ncx[0] == 'section':
+                sectoc = toc.add(unicode(
+                    ncx[1]), ncx[2], klass='section',
+                    play_order=po, id='Main-section-%d' % po,
+                    toc_thumbnail=toc_thumbnail)
+            elif sectoc:
+                sectoc.add(unicode(
+                    ncx[1]), ncx[2], description=ncx[3] if ncx[3] else None,
+                    klass='article', play_order=po,
+                    id='article-%d' % po, toc_thumbnail=toc_thumbnail)
+            po += 1
 
     @abc.abstractmethod
     def generate(self, data, spider, usermeta, *args, **kwargs):
