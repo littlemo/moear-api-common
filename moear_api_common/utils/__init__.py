b603ad8a5a2c34efd2bdb281da5f07ec57370807
Moore.Huang <moore@moorehy.com>
2018-03-13 16:43:45 +0800

    F: 实现创建路径树的工具方法

---
+++ moear_api_common/utils/__init__.py
@@ -0,0 +1,29 @@
+import os
+import errno
+
+
+def mkdirp(path):
+    """
+    创建传入的路径树
+
+    Args:
+        path (str):
+            Path containing directories to create
+
+    Returns:
+        str: The passed in path
+
+    Raises:
+        OSError:
+            If some underlying error occurs when calling :func:`os.makedirs`,
+            that is not errno.EEXIST.
+    """
+    try:
+        os.makedirs(path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+    return path
