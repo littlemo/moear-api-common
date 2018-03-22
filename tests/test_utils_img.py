345892a9920d812f24afb77fb6790c4d70a72360
Moore.Huang <moore@moorehy.com>
2018-03-22 17:53:47 +0800

    F: 实现工具处理方法的单元测试

---
+++ tests/test_utils_img.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import logging
+import unittest
+import shutil
+
+from moear_api_common import utils
+from moear_api_common.utils import img
+from moear_api_common import config
+
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+format = logging.Formatter("%(asctime)s - %(message)s")  # output format
+sh = logging.StreamHandler(stream=sys.stdout)  # output to standard output
+sh.setFormatter(format)
+log.addHandler(sh)
+
+_base_dir = os.path.dirname(os.path.abspath(__file__))
+_assets_dir = os.path.join(_base_dir, 'assets')
+_build_dir = os.path.join(_base_dir, 'build')
+
+
+class TestUtilsImgMethods(unittest.TestCase):
+    """
+    测试工具包中img的工具方法
+    """
+    def test_000_rescale_image(self):
+        """测试调整图片工具方法"""
+        raw_path = os.path.join(_assets_dir, 'raw.jpg')
+        with open(raw_path, 'rb') as fh:
+            raw_img_buf = fh.read()
+        data = img.rescale_image(
+            raw_img_buf,
+            dimen=config.img_max_thumb_dimen,
+            maxsizeb=config.img_max_thumb_size)
+        output_path = os.path.join(_build_dir, 'output.jpg')
+        shutil.rmtree(_build_dir, ignore_errors=True)
+        utils.mkdirp(_build_dir)
+        with open(output_path, 'wb') as fh:
+            fh.write(data)
