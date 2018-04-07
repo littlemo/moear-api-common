2be54167365ecf7f1bb26b39978ac4008f8af874
Moore.Huang <moore@moorehy.com>
2018-04-07 17:14:41 +0800

    O: 优化接口类中的文档格式，使其符合sphinx的automodule相关语法定义

--- moear_api_common/base.py
+++ moear_api_common/base.py
@@ -9,7 +9,6 @@ from .utils import get_config_dict
 class SpiderBase(object):
     """
     爬虫基类
-    ========
 
     用以作为抽象类定义爬虫扩展所需提供的服务接口
 
@@ -24,9 +23,9 @@ class SpiderBase(object):
         """
         初始化默认配置参数，可在子类中进行覆盖
 
-        配置优先级为：用户元数据 > 具体Package配置 > Common全局默认配置
+        配置优先级为：``用户元数据`` > ``具体Package配置`` > ``Common全局默认配置``
 
-        :params usermeta dict: （可选，关键字参数）指定用户的package相关配置元数据，
+        :param dict usermeta: （*可选*，关键字参数）指定用户的package相关配置元数据，
             如：定制书籍名(book_title)等
         """
         # 设置全局默认配置
@@ -43,7 +42,6 @@ class SpiderBase(object):
     def __configure(self, options):
         """
         更新自定义配置
-        --------------
 
         该方法用于更新配置信息，Package开发者可在实现具体类时在 ``__init__`` 中进行调用，
         从而覆盖全局配置。
@@ -57,48 +55,48 @@ class SpiderBase(object):
     def hook_custom_options(self):
         """
         配置定制配置项钩子
-        ------------
 
         该方法返回当前类的自定义配置项，由基类在 ``__init__`` 方法中调用，
         调用点位于，Common默认全局配置完成后，用户元数据配置前
 
-        :returns: dict, 返回当前类的自定义配置项
+        :return: 返回当前类的自定义配置项
+        :rtype: dict
         """
 
     @abc.abstractmethod
     def register(self, *args, **kwargs):
         """
         注册
-        ----
 
         调用方可根据主键字段进行爬虫的创建或更新操作
 
-        :returns: dict, 返回符合接口定义的字典数据
+        :return: 返回符合接口定义的字典数据
+        :rtype: dict
         """
 
     @abc.abstractmethod
     def crawl(self, *args, **kwargs):
         """
         爬取
-        ----
 
         执行爬取操作，并阻塞直到爬取完成，返回结果数据
 
-        :returns: dict, 返回符合接口定义的字典数据
+        :return: 返回符合接口定义的字典数据
+        :rtype: dict
         """
 
     @abc.abstractmethod
     def format(self, data, *args, **kwargs):
         """
         格式化
-        ------
 
         将传入的Post列表数据进行格式化处理
 
         :param data: 待处理的文章列表
         :type data: list
 
-        :returns: dict, 返回符合mobi打包需求的定制化数据结构
+        :return: 返回符合mobi打包需求的定制化数据结构
+        :rtype: dict
         """
 
 
@@ -106,7 +104,6 @@ class SpiderBase(object):
 class PackageBase(object):
     """
     打包基类
-    ========
 
     用以作为抽象类定义打包驱动所需提供的服务接口
     """
@@ -114,11 +111,12 @@ class PackageBase(object):
         """
         初始化默认配置参数，可在子类中进行覆盖
 
-        配置优先级为：用户元数据 > Spider元数据 > 具体Package配置 > Common全局默认配置
+        配置优先级为：``用户元数据`` > ``Spider元数据`` >
+        ``具体Package配置`` > ``Common全局默认配置``
 
-        :params spider dict: （必须）指定爬虫的信息数据(包括 'meta' 字段的元数据字典，
+        :param dict spider: 指定爬虫的信息数据(包括 'meta' 字段的元数据字典，
             其中需包含书籍名称用的时间戳)
-        :params usermeta dict: （可选，关键字参数）指定用户的package相关配置元数据，
+        :param dict usermeta: （*可选*，关键字参数）指定用户的package相关配置元数据，
             如：定制书籍名(book_title)等
         """
         # 设置全局默认配置
@@ -139,7 +137,6 @@ class PackageBase(object):
     def __configure(self, options):
         """
         更新自定义配置
-        --------------
 
         该方法用于更新配置信息，Package开发者可在实现具体类时在 ``__init__`` 中进行调用，
         从而覆盖全局配置。
@@ -168,23 +165,23 @@ class PackageBase(object):
     def hook_custom_options(self):
         """
         配置定制配置项钩子
-        ------------
 
         该方法返回当前类的自定义配置项，由基类在 ``__init__`` 方法中调用，
         调用点位于，Common默认全局配置完成后，Spider元数据、用户元数据配置前
 
-        :returns: dict, 返回当前类的自定义配置项
+        :return: 返回当前类的自定义配置项
+        :rtype: dict
         """
 
     @abc.abstractmethod
     def generate(self, data, *args, **kwargs):
         """
         生成
-        ----
 
         根据传入的数据结构生成最终用于推送的文件字节字符串对象(byteStringIO)，
         MoEar会将其持久化并用于之后的推送任务
 
-        :params data dict: 待打包的数据结构
-        :returns: (bytes, ext), 返回生成的书籍打包输出字节与格式扩展名
+        :param dict data: 待打包的数据结构
+        :return: 返回生成的书籍打包输出字节
+        :rtype: bytes
         """
