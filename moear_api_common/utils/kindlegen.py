047592a6b3588b59b7057635d2022fbd81694e49
Moore.Huang <moore@moorehy.com>
2018-04-08 10:35:46 +0800

    O: 优化kindlegen包中的方法说明文档

--- moear_api_common/utils/kindlegen.py
+++ moear_api_common/utils/kindlegen.py
@@ -4,17 +4,17 @@ import os
 def find_kindlegen_prog(path=None):
     '''
     获取kindlegen程序路径
-    ---------------------
 
     该方法会检索系统 ``PATH`` 路径，查找包含 ``kindlegen`` 文件的路径，
     并返回该文件的绝对路径。若不存在则返回 ``None``
 
-    .. attentions::
+    .. attention::
 
         检索 ``PATH`` 路径前，会优先寻找指定路径，并在检索到第一个符合文件后立刻返回
 
-    :param path str: 关键字参数，若传入，则优先检测该路径下是否存在 kindlegen 程序
-    :returns: str, kindlegen的绝对路径
+    :param str path: 关键字参数，若传入，则优先检测该路径下是否存在 kindlegen 程序
+    :return: kindlegen的绝对路径
+    :rtype: str
     '''
     try:
         kindlegen_prog = 'kindlegen'
