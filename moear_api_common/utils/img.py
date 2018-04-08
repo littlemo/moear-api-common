0cdb1c219cc19c32a0981eedcbffe183e53dff1e
Moore.Huang <moore@moorehy.com>
2018-04-08 11:03:46 +0800

    O: 优化整理img工具包中的工具方法说明文档

--- moear_api_common/utils/img.py
+++ moear_api_common/utils/img.py
@@ -6,15 +6,23 @@ def rescale_image(
         data, maxsizeb=4000000, dimen=None,
         png2jpg=False, graying=True, reduceto=(600, 800)):
     '''
-    Convert image setting all transparent pixels to white and changing format
-    to JPEG. Ensure the resultant image has a byte size less than
-    maxsizeb.
+    若 ``png2jpg`` 为 ``True`` 则将图片转换为 ``JPEG`` 格式，所有透明像素被设置为
+    *白色* 。确保结果图片尺寸小于 ``maxsizeb`` 的约束限制。
 
-    If dimen is not None, generate a thumbnail of
-    width=dimen, height=dimen or width, height = dimen (depending on the type
-    of dimen)
+    如果 ``dimen`` 不为空，则生成一个相应约束的缩略图。依据 ``dimen`` 的类型，设置约束为
+    ``width=dimen, height=dimen`` 或者 ``width, height = dimen``
 
-    Returns the image as a bytestring.
+    :param data: 原始图片字节数据
+    :type data: bytes or io.BytesIO
+    :param int maxsizeb: 文件大小约束，单位：字节
+    :param dimen: 缩略图尺寸约束，宽&高
+    :type dimen: int or (int, int)
+    :param bool png2jpg: 是否将图片转换为 JPG 格式
+    :param bool graying: 是否将图片进行灰度处理
+    :param reduceto: 若图片大于此约束则进行相应缩小处理，宽&高
+    :type reduceto: (int, int)
+    :return: 处理后的图片字节数据，可直接以 ``wb`` 模式输出到文件中
+    :rtype: bytes
     '''
     if not isinstance(data, BytesIO):
         data = BytesIO(data)
@@ -62,8 +70,9 @@ def gray_image(data):
 
     将传入的图片数据转换为灰度图后返回
 
-    :param data bytes: 图片字节数据，可通过以 'rb' 模式读取图片文件获得
-    :return: 处理后的图片字节数据，可直接以 'wb' 模式输出到文件中
+    :param data bytes: 图片字节数据，可通过以 ``rb`` 模式读取图片文件获得
+    :return: 处理后的图片字节数据，可直接以 ``wb`` 模式输出到文件中
+    :rtype: bytes
     '''
     if not isinstance(data, BytesIO):
         data = BytesIO(data)
